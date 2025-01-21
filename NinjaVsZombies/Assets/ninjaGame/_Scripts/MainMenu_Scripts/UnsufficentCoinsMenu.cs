4a0c294acaa7c44fae2c1a359b4c2111dea313b9
CZY <chenzhengyoung@gmail.com>
2025-01-21 20:20:02 +0800

    1.0

---
+++ NinjaVsZombies/Assets/ninjaGame/_Scripts/MainMenu_Scripts/UnsufficentCoinsMenu.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnsufficentCoinsMenu : MonoBehaviour
+{
+
+	public GameObject PlayerSelectionMenu, UnsufficentMenu,PlayerGroup;//for inapp,unsufficent menu screens
+	void Start()
+	{
+
+	}
+	void Update()
+	{
+
+	}
+
+
+	//for button control
+	public void OnButtonClick(string ButtonName)
+	{
+		switch (ButtonName)
+		{
+			//for ok button
+			case "Ok":
+				AdManager.ShowVideoAd("192if3b93qo6991ed0",
+			(bol) => {
+				if (bol)
+				{
+					TotalCoins.Static.AddCoins(500);
+					TotalCoins.Static.UpdateCoins();
+					UnsufficentMenu.SetActive(false);//for unsufficent menu disables
+					PlayerSelectionMenu.SetActive(true);
+					PlayerGroup.SetActive(true);
+					SoundController.Static.PlayClickSound();//for click sound
+					MainMenuScreens.currentScreen = MainMenuScreens.MenuScreens.playerSelectionMenu;//for moving inapp menu state
+
+					AdManager.clickid = "";
+					AdManager.getClickid();
+					AdManager.apiSend("game_addiction", AdManager.clickid);
+					AdManager.apiSend("lt_roi", AdManager.clickid);
+
+
+				}
+				else
+				{
+					StarkSDKSpace.AndroidUIManager.ShowToast("观看完整视频才能获取奖励哦！");
+				}
+			},
+			(it, str) => {
+				Debug.LogError("Error->" + str);
+				//AndroidUIManager.ShowToast("广告加载异常，请重新看广告！");
+			});
+				break;
+			case "NO":
+				UnsufficentMenu.SetActive(false);//for unsufficent menu disables
+				PlayerSelectionMenu.SetActive(true);
+				PlayerGroup.SetActive(true);
+				SoundController.Static.PlayClickSound();//for click sound
+				MainMenuScreens.currentScreen = MainMenuScreens.MenuScreens.playerSelectionMenu;//for moving inapp menu state
+				break;
+		}
+	}
+}
